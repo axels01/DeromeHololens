108918fa755b50624318d15997617790d7b927b7
Axel <45068961+axels01@users.noreply.github.com>
2023-02-16 21:05:44 +0100

    Updated comments
    Code not tested since last commit

--- Assets/Script/FileSelector.cs
+++ Assets/Script/FileSelector.cs
@@ -11,10 +11,10 @@ using UIManager;
 
 public class FileSelector : MonoBehaviour
 {
-    //Button prefab
+    //Button prefab.
     public GameObject prefab;
 
-    //General IO objects
+    //General IO objects.
     public GameObject mainUI;
     public GameObject parent;
     public GameObject backButton;
@@ -22,36 +22,47 @@ public class FileSelector : MonoBehaviour
     public GameObject searchButton;
     public GridObjectCollection collection;
 
-    //Use selected file
+    //"Use selected file" screen objects.
     public GameObject noButton;
     public GameObject yesButton;
     public GameObject useFilePrompt;
     public GameObject useFileScreen;
 
-    //Instances of directoryManager
+    /*Instances of directoryManager, only keeps current and start alive due to 
+    this projects application. If you want to keep multiple instances of currentDirectory
+    alive for future use they would be added to a stack or similar.
+    */
     directortManager startDirectory;
     directortManager currentDirectory;
-    //Path to read from, will changed to be read from a config file
-    private string startPath = @"C:\Users\Axel\Desktop\DeromeTruss";
-    //Stores the history of paths, push when entering a new path, pop when going back
     Stack pathHistory = new Stack();
+    
+    private string startPath = @"C:\Users\Axel\Desktop\DeromeTruss";
     private Dictionary<GameObject, UIButton> buttons = new Dictionary<GameObject, UIButton>();
-
+    
+    
+    //Initializer, runs once.
     void Start()
     {
-        //Sets the correct UI visible
+        //Sets the correct UI screen/view visible.
         useFileScreen.SetActive(false);
         mainUI.SetActive(true);
 
 
-        //Starts a new ButtonHandler for the start directroy
+        /*Starts an instance of directoryManager for the start directory which is kept alive
+        as startDirectory, although the instance which will be used in browsing is currentDirectory.
+        */
         startDirectory = new directortManager(startPath, parent, prefab);
         startDirectory.updateCollection();
         currentDirectory = startDirectory;
 
 
 
-        //Populates buttons with instances of buttonManager for the buttons.
+        /*Creates instances of UIButton datatype for each button and adds to buttons dictionary.
+        Loops through the dictionary and assigns a instance of buttonManager and pressState to 
+        each button. 
+        
+        Uses a fake constructor for buttonManager, which is further explained in ButtonManager.cs. 
+        */
         buttons.Add(noButton, new UIButton());
         buttons.Add(yesButton, new UIButton());
         buttons.Add(backButton, new UIButton());
@@ -61,14 +72,20 @@ public class FileSelector : MonoBehaviour
             button.Value.buttonManager = new buttonManager();
             button.Value.pressState = button.Value.buttonManager.buttonManagerBuilder(button.Key);
         }
-
-        buttons[noButton].pressState = buttons[noButton].buttonManager.buttonManagerBuilder(noButton);
     }
 
+    //Update runs once per frame.
     void Update()
     {
+        /*If statement regarding which screen/view should be show, should probably be replacd with switch case.
+        mainUI represents the view/screen showing on start.
+        */
         if (mainUI.activeSelf)
         {
+            /*When going back in the directory tree, pathHistory.Count != 0 makes sure
+            it cant go back past the start directroy. Sets the old instance of DirectoryManager
+            to unactive before assigning currentDirectory a new instance of DirectoryManager.
+            */
             if (buttons[backButton].buttonManager.pressed(buttons[backButton].pressState))
             {
                 Debug.Log("Back!");
@@ -79,19 +96,24 @@ public class FileSelector : MonoBehaviour
                     currentDirectory.setActive(true);
                 }
             }
-
+            
+            /*Runs buttonStatus of DirectoryManager currently assigned to currentDirectory
+            to check whether a button has been pressed, returns dict which is captured in dirctoryButton.
+            Checks whether file or directory, 
+            
+            if file, make sure it's of typ .dxf and change view/screen to useFileScreen.
+            
+            if directory, set the setActive of the view of the current directory false, push the new
+            directory path to stack, assign currentDirectory new instance of DirectoryManager with
+            updated path. Set setActive true for the new view.
+            */
             Dictionary<string, string> directoryButton = currentDirectory.buttonStatus();
+            
             if (directoryButton != null)
             {
                 if (directoryButton["Type"] == "file")
                 {
                     Debug.Log(directoryButton["Name"] + " : " + directoryButton["Type"]);
-                    /*Add logic 
-                     * Check if the file is .dxf
-                     * if it is, pass to ParseDXF and set this script inactive
-                     */
-
-                    //if file ends with .dxf, ignoreCase = true, culture = null
                     if (directoryButton["Name"].EndsWith(".dxf", true, null))
                     {
                         mainUI.SetActive(false);
@@ -101,25 +123,18 @@ public class FileSelector : MonoBehaviour
 
                 if (directoryButton["Type"] == "directory")
                 {
-                    /*if the button pressed is a directory, set the currentDirectory
-                    *to false, this only makes a difference if the user is currently standing in
-                    *startDirectory as the buttons from said directory will be shown alongside the
-                    *ones from the new directory
-                    */
                     Debug.Log(directoryButton["Name"] + " : " + directoryButton["Type"]);
                     currentDirectory.setActive(false);
-
-                    /*starts a new instance of ButtonHandler under currentDirectory variable with
-                    *the new path, then updates then updateCollection and setActive for the buttons
-                    *from the new directory
-                    */
                     pathHistory.Push(currentDirectory.directoryPath);
                     currentDirectory = new directortManager(directoryButton["Path"], parent, prefab);
                     currentDirectory.setActive(true);
                 }
             }
         }
-
+        
+        /*View/screen when user has clicked on a file.
+        Allows user to cancel or contine via yes/no buttons.
+        */
         if (useFileScreen.activeSelf)
         {
             if (buttons[noButton].buttonManager.pressed(buttons[noButton].pressState))
@@ -137,6 +152,7 @@ public class FileSelector : MonoBehaviour
     }
 }
 
+//UIbutton datatype.
 class UIButton
 {
     public buttonManager buttonManager { get; set; }
